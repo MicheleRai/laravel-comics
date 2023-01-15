5deb3ed9f183cc38a5b2295b4ce91164e6d7a2cd
MicheleRai <rainonemichele1999@gmail.com>
2023-01-15 20:38:23 +0100

    img logo added

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container-header">
-        <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+        <a href="{{'/'}}"><img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo"></a>
         @include('guest.partials.navbar')
     </div>
 </header>
